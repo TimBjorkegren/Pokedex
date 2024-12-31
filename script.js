a78e7ba68f120d690844c8cf9a48adeb0b111c89
Tim <tim.bjorkegren@gmail.com>
2024-12-31 14:35:17 +0100

    Loaded in pokemons via advenlistener
    Pokemons load in directly when the site start so the user can look at
    all the pokemons directly. Used adventlistner on DOMContentLoaded and
    the function loadAllPokemons so it starts direct

--- script.js
+++ script.js
@@ -14,11 +14,6 @@ async function fetchData() {
 
         const pokemonSprite = data.sprites.front_default;
         const pokemonNameFormatted = data.name.charAt(0).toUpperCase() + data.name.slice(1);
-        const imgElement = document.getElementById("pokemonSprite");
-
-        /*
-        imgElement.src = pokemonSprite;
-        imgElement.style.display = "block"; */
 
         const container = document.getElementById("pokemonContainer");
         container.innerHTML = "";
@@ -38,3 +33,40 @@ async function fetchData() {
         alert("Pokemon was not found!")
     }
 }
+
+async function loadAllPokemon() {
+    try {
+        const container = document.getElementById("pokemonContainer");
+        container.innerHTML = "Loading...";
+
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
+        const data = await response.json();
+
+        container.innerHTML = "";
+
+        for (const pokemon of data.results) {
+            const pokemonDeatils = await fetch(pokemon.url);
+            const detailsData = await pokemonDeatils.json();
+
+
+            const pokemonSprite = detailsData.sprites.front_default;
+            const pokemonNameFormatted = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+
+            const card = document.createElement("div");
+            card.className = "Pokemon-Card";
+
+            card.innerHTML = `
+        <img src ="${pokemonSprite}">
+        <p>"${pokemonNameFormatted}"</p>
+        `;
+
+            container.appendChild(card)
+
+        }
+    } catch (error) {
+        console.error(error);
+        alert("Could not load Pokemon!");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", loadAllPokemon);
